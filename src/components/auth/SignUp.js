3c093ae374c2f6100e32f265e893bd1fcfbd9133
AJ Speller <ajspeller@gmail.com>
2019-05-06 00:15:23 -0400

    Add/Edit components App, SingIn and SignUp.  Added javascript tag for materializecss

---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+export default class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      firstName: '',
+      lastName: ''
+    };
+    this.submitHandler = this.submitHandler.bind(this);
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+  submitHandler(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  changeHandler(e) {
+    this.setState({ [e.target.id]: e.target.value });
+  }
+  render() {
+    return (
+      <div className='container'>
+        <form className='white' onSubmit={this.submitHandler}>
+          <h5 className='grey-text text-darken-3'>Sign Up</h5>
+          <div className='input-field'>
+            <label htmlFor='email'>Email</label>
+            <input
+              id='email'
+              type='email'
+              onChange={this.changeHandler}
+              value={this.state.email}
+            />
+          </div>
+          <div className='input-field'>
+            <label htmlFor='password'>Password</label>
+            <input
+              id='password'
+              type='password'
+              onChange={this.changeHandler}
+              value={this.state.password}
+            />
+          </div>
+          <div className='input-field'>
+            <label htmlFor='firstName'>First Name</label>
+            <input
+              id='firstName'
+              type='text'
+              onChange={this.changeHandler}
+              value={this.state.firstName}
+            />
+          </div>
+          <div className='input-field'>
+            <label htmlFor='lastName'>Last Name</label>
+            <input
+              id='lastName'
+              type='text'
+              onChange={this.changeHandler}
+              value={this.state.lastName}
+            />
+          </div>
+          <div className='input-field'>
+            <button className='btn pink lighten-1 z-depth-0'>Login</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
