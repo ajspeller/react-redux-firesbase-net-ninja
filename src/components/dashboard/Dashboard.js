fd6ffa2c62dd7ea4615b2973285bc4c530d8e767
AJ Speller <ajspeller@gmail.com>
2019-05-05 23:18:00 -0400

    Add/Edit components: Dashboard, Notifications and Project List

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+// -- component
+import Notifications from './Notifications';
+import ProjectList from '../project/ProjectList';
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className='dashboard container'>
+        <div className='row'>
+          <div className='col s12 m6'>
+            <ProjectList />
+          </div>
+          <div className='col s12 m5 offset-m1'>
+            <Notifications />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
---
+++ src/components/project/ProjectList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ProjectList = () => {
+  return (
+    <div className='project-list section'>
+      <div className='card z-depth-0 project-summary'>
+        <div className='card-content grey-text darken-3'>
+          <span className='card-title'>Project Title</span>
+          <p>Posted by AJ Speller</p>
+          <div className='grey-text'>3rd May, 2am</div>
+        </div>
+      </div>
+      <div className='card z-depth-0 project-summary'>
+        <div className='card-content grey-text darken-3'>
+          <span className='card-title'>Project Title</span>
+          <p>Posted by AJ Speller</p>
+          <div className='grey-text'>3rd May, 2am</div>
+        </div>
+      </div>
+      <div className='card z-depth-0 project-summary'>
+        <div className='card-content grey-text darken-3'>
+          <span className='card-title'>Project Title</span>
+          <p>Posted by AJ Speller</p>
+          <div className='grey-text'>3rd May, 2am</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
